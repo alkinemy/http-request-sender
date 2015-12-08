87b35192f5d5e1e0ef6e7a83e314e02098cf11d0
joke111 <alta.second@gmail.com>
2015-12-08 15:11:40 +0900

    Implement tcp client

--- client/src/joke/client/TcpClient.java
+++ client/src/joke/client/TcpClient.java
@@ -3,37 +3,33 @@ package joke.client;
 import joke.message.request.Request;
 import joke.message.response.Response;
 
-import java.io.Closeable;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 public class TcpClient {
+
 	public Response connectAndSend(Request request) {
-		DataOutputStream outputStream = null;
-		try (Socket socket = new Socket(request.getAddress(), request.getPort())) {
-			outputStream = new DataOutputStream(socket.getOutputStream());
+		try (Socket socket = new Socket(request.getAddress(), request.getPort());
+			OutputStream socketOutputStream = new BufferedOutputStream(new DataOutputStream(socket.getOutputStream()));
+			InputStream socketInputStream = new BufferedInputStream(socket.getInputStream());
+			ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
 
-			outputStream.writeUTF(request.getMessage());
+			socketOutputStream.write(request.getMessage().getBytes(Charset.forName("UTF-8")));
+			socketOutputStream.flush();
 
-			outputStream.flush();
-			outputStream.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			closeQuietly(outputStream);
-		}
-
-		return null; //TODO Response를 리턴하도록 처리
-	}
-
-	private void closeQuietly(Closeable closeable) {
-		try {
-			if (closeable != null) {
-				closeable.close();
+			int data;
+			while((data = socketInputStream.read()) != -1) {
+				bos.write(data);
 			}
+			byte[] bytes = bos.toByteArray();
+			System.out.println(new String(bytes, Charset.forName("UTF-8")));
+
+			return null;
+
 		} catch (IOException e) {
 			e.printStackTrace();
+			return null;
 		}
 	}
 
