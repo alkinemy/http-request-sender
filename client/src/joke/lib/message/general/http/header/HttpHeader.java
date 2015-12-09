a245042f7f13e8a48a73b7e613c71cbe2b268bbb
joke111 <alta.second@gmail.com>
2015-12-09 14:32:04 +0900

    Change header value type

--- client/src/joke/lib/message/general/http/header/HttpHeader.java
+++ client/src/joke/lib/message/general/http/header/HttpHeader.java
@@ -3,21 +3,25 @@ package joke.lib.message.general.http.header;
 public class HttpHeader {
 
 	private String name;
-	private String value;
+	private Object value;
 
-	public static HttpHeader of(String name, String value) {
+	public static HttpHeader of(String name, Object value) {
 		HttpHeader header = new HttpHeader();
 		header.setName(name);
 		header.setValue(value);
 		return header;
 	}
 
-	public String getValue() {
+	public Object getValue() {
 		return value;
 	}
 
-	public void setValue(String value) {
+	public void setValue(Object value) {
 		this.value = value;
+	}
+
+	public String getStringValue() {
+		return value.toString();
 	}
 
 	public String getName() {
